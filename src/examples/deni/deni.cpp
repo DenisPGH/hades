df73605e3c4ee0d53b6e1346fdad3303ecc36775
nano <nano@nano-Ubuntu.(none)>
2023-04-28 12:05:09 +0200

    Revert "deleted my other unnecessary directories"
    This reverts commit 9a618db9e6dee92510e7acf18ed6b958fe924486.

---
+++ src/examples/deni/deni.cpp
@@ -0,0 +1,116 @@
+
+
+/**
+ * @file deni_app.c
+ * Minimal application example for PX4 autopilot
+ *
+ * @author Example User <mail@example.com>
+ */
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/tasks.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/module.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <poll.h>
+#include <string.h>
+#include <math.h>
+
+
+#include "deni.hpp"
+#include <uORB/topics/differential_pressure.h>
+
+//make
+// PATHS
+//  /home/nano/Firmware/src/modules/mavlink/mavlink_messages.cpp
+//  /home/nano/Firmware/src/modules/mavlink/streams/VFR_HUD.hpp
+//  /home/nano/Firmware/build/px4_sitl_default/mavlink/common/mavlink_msg_vfr_hud.h
+//  /home/nano/Firmware/platforms/common/uORB/Subscription.hpp
+//#include <platforms/common/uORB/uORB.h>
+// /home/nano/Firmware/platforms/common/uORB/Publication.hpp
+//#include <msg/topics_sources/uORBTopics.cpp>
+//  /home/nano/Firmware/build/px4_sitl_default/msg/topics_sources/airspeed.cpp
+//  /home/nano/Firmware/msg/Airspeed_ASP503.msg
+//#include <modules/mavlink/streams/VFR_HUD.hpp>
+///  /home/nano/Firmware/msg/CMakeLists.txt
+// /home/nano/Firmware/build/px4_sitl_default/src/examples/deni
+// /home/nano/Firmware/build/px4_sitl_default/compile_commands.json
+// /home/nano/Firmware/build/px4_sitl_default/platforms/posix/apps.cpp
+
+// /home/nano/Firmware/build/px4_sitl_default/uORB/topics/differential_pressure.h
+//#include <modules/mavlink/streams/SCALED_PRESSURE3.hpp>
+//  /home/nano/Firmware/platforms/common/include/px4_platform_common/module.h
+//  /home/nano/Firmware/ROMFS/px4fmu_common/init.d/rcS
+// /home/nano/Firmware/ROMFS/px4fmu_common/init.d-posix/rcS
+// /home/nano/Firmware/ROMFS/cannode/init.d/rcS
+// /home/nano/Firmware/posix-configs/SITL/init/test/test_cmd_template.in
+// /home/nano/Firmware/posix-configs/SITL/init/test/test_mavlink
+
+
+// /home/nano/Firmware/ROMFS/px4fmu_common/init.d/rcS == this file run my script in the PC
+// /home/nano/Firmware/boards/px4/fmu-v6c/default.px4board need this for Pixhawk 6c
+
+// /home/nano/Firmware/Tools/upload.sh --> start upload
+// asp5033driver #0 on I2C bus 4 address 0x6D
+// /home/nano/Firmware/src/drivers/drv_sensor.h
+
+/* After adding a new parameter file you should call make clean */
+
+// /home/nano/Firmware/boards/ark/cannode/init/rc.board_sensors
+// /home/nano/Firmware/src/modules/commander/airspeed_calibration.cpp
+///home/nano/Firmware/boards/px4/fmu-v6c/init/rc.board_sensors - here the sensor start automaticaly
+
+// /home/nano/Firmware/src/lib/drivers/device/posix/I2C.cpp i2c communication file
+
+///home/nano/Firmware/platforms/nuttx/NuttX/nuttx/arch/arm/include/stm32/chip.h
+
+
+
+class Air: public ASP5033Driver {
+	public:
+	int main(int argc, char *argv[]){
+
+
+		struct differential_pressure_s asp5033_;
+
+		//PX4_INFO("before temp: %8.4f ms",(double)asp5033_.temperature);
+		//PX4_INFO(" press: %8.4f ms",(double)asp5033_.differential_pressure_pa);
+
+		memset(&asp5033_, 0, sizeof(asp5033_));
+		orb_advert_t asp5033_pub = orb_advertise(ORB_ID(differential_pressure), &asp5033_);
+		//orb_copy(ORB_ID(airspeed__a_s_p5033), asp5033_pub, &get_temp());
+		//while (){
+		asp5033_.temperature=4.0;
+		asp5033_.differential_pressure_pa=200;
+		orb_publish(ORB_ID(differential_pressure), asp5033_pub, &asp5033_);
+
+		//}
+
+		PX4_INFO("ASP5033 temp: %8.4f ms",(double)asp5033_.temperature);
+		PX4_INFO("ASP5033 press: %8.4f ms",(double)asp5033_.differential_pressure_pa);
+
+
+
+		return 0;
+
+	}
+
+
+};
+
+
+
+
+
+
+
+/* from this extern part got the main program and cant print*/
+extern "C" __EXPORT int deni_main(int argc, char *argv[])
+{
+	Air fk;
+	return fk.main(argc, argv);
+}
+
+
